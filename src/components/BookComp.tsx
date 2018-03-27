7e84fe39521b5c3479e07c6e1885e304a204912a
Anton Shakirzianov <ashakirzianov@icloud.com>
2018-03-27 16:50:20 -0700

    Showing titles for all book parts

--- src/components/BookComp.tsx
+++ src/components/BookComp.tsx
@@ -5,30 +5,37 @@ import { TextBlock, Column, Title } from './Elements';
 import { assertNever } from '../utils';
 
 const ParagraphComp: Comp<{ p: Paragraph }> = props =>
-    <TextBlock text={props.p}/>;
+    <TextBlock text={props.p} />;
 
 const SubpartComp: Comp<SubPart> = props =>
     <Column>
-        { props.content.map(bn => <BookNodeComp node={bn}/>) }
+        <Title text={props.title} />
+        {props.content.map(bn => <BookNodeComp node={bn} />)}
     </Column>;
 
 const PartComp: Comp<Part> = props =>
-    <Column>{ props.content.map(bn => <BookNodeComp node={bn}/>) }</Column>;
+    <Column>
+        <Title text={props.title} />
+        {props.content.map(bn => <BookNodeComp node={bn} />)}
+    </Column>;
 
 const ChapterComp: Comp<Chapter> = props =>
     <Column>
-        <Title text={props.title}/>
-        { props.content.map(bn => <BookNodeComp node={bn}/>) }
+        <Title text={props.title} />
+        {props.content.map(bn => <BookNodeComp node={bn} />)}
     </Column>;
 
 const BookNodeComp: Comp<{ node: BookNode }> = props =>
-    typeof props.node === 'string' ? <ParagraphComp p={props.node}/>
-    : props.node.kind === 'chapter' ? <ChapterComp {...props.node} />
-    : props.node.kind === 'part' ? <PartComp {...props.node} />
-    : props.node.kind === 'subpart' ? <SubpartComp {...props.node} />
-    : assertNever(props.node);
+    typeof props.node === 'string' ? <ParagraphComp p={props.node} />
+        : props.node.kind === 'chapter' ? <ChapterComp {...props.node} />
+            : props.node.kind === 'part' ? <PartComp {...props.node} />
+                : props.node.kind === 'subpart' ? <SubpartComp {...props.node} />
+                    : assertNever(props.node);
 
 const BookComp: Comp<Book> = props =>
-    <Column>{ props.content.map(bn => <BookNodeComp node={bn} />) }</Column>;
+    <Column>
+        <Title text={props.title} />
+        {props.content.map(bn => <BookNodeComp node={bn} />)}
+    </Column>;
 
 export { BookComp };
