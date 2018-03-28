a975372dcbc25fea20fb6de67da731225d888bc5
Anton Shakirzianov <ashakirzianov@icloud.com>
2018-03-27 17:56:47 -0700

    Part, Chapter & Subpart title styles

--- src/components/Elements.tsx
+++ src/components/Elements.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as Atoms from './Atoms';
+// import * as Atoms from './Atoms';
 import { Text, View } from './Atoms';
 import { Comp } from './comp-utils';
 
@@ -10,16 +10,22 @@ export const Column: Comp = props =>
     <View style={{ flexDirection: 'column' }}>{props.children}</View>;
 
 export const ChapterTitle: Comp<{ text: string }> = props =>
-    <Atoms.Text>{props.text}</Atoms.Text>;
+    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 16 }}>{props.text}</Text>
+    </View>;
 
 export const PartTitle: Comp<{ text: string }> = props =>
-    <Atoms.Text>{props.text}</Atoms.Text>;
+    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{props.text}</Text>
+    </View>;
 
 export const SubpartTitle: Comp<{ text: string }> = props =>
-    <Atoms.Text>{props.text}</Atoms.Text>;
+    <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+        <Text style={{ fontWeight: 'bold', fontSize: 12 }}>{props.text}</Text>
+    </View>;
 
 export const BookTitle: Comp<{ text: string }> = props =>
-    <View style={{ flexDirection: 'row', justifyContent: 'center'}}>
+    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
         <Text style={{ fontWeight: 'bold', fontSize: 24 }}>{props.text}</Text>
     </View>;
 
