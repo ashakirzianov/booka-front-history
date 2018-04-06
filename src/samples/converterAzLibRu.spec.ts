9424d86719c952a3789ef03ab81edb2b05406b02
Anton Shakirzianov <ashakirzianov@icloud.com>
2018-04-05 21:18:31 -0700

    Full version of 'az.lib.ru' html to xml converter

--- src/samples/converterAzLibRu.spec.ts
+++ src/samples/converterAzLibRu.spec.ts
@@ -1,28 +1,7 @@
-import { parseHtml, htmlToWellFormedXml } from './converterAzLibRu';
+import { parseHtml } from './converterAzLibRu';
 import { text } from './warAndPeace';
-import { parseXml } from '../xmlParser';
-
-it.only('Test html->xml converting', () => {
-    // debugger; // tslint:disable-line
-    const htmlExample = `
-    <center>
-<table width='90%' border='0' cellpadding=0 cellspacing=0>
-
-</table>
-
-<hr noshade size=2>
-
-</center>
-`;
-    const wellXml = htmlToWellFormedXml(htmlExample);
-    console.log(wellXml); // tslint:disable-line
-    const wpXml = parseXml(wellXml);
-    expect(wpXml).toBeDefined();
-    expect(wpXml.type).toBe('document');
-});
 
 it('War and Peace parsing', () => {
-    debugger; // tslint:disable-line
     const wpXml = parseHtml(text);
     expect(wpXml).toBeDefined();
     expect(wpXml.type).toBe('document');
--- src/samples/converterAzLibRu.ts
+++ src/samples/converterAzLibRu.ts
@@ -1,30 +1,46 @@
 import { parseXml } from '../xmlParser';
 import { combineFs } from '../utils';
 
+function multiRun(f: (s: string) => string) {
+    return (input: string) => {
+        let next = input;
+        let current;
+        do {
+            debugger; // tslint:disable-line
+            current = next;
+            next = f(current);
+        } while (current.length !== next.length);
+        return next;
+    };
+}
+
 export function parseHtml(html: string) {
     const xml = htmlToWellFormedXml(html);
     return parseXml(xml);
 }
 
 export function fixAllUnquotedAttributes(html: string) {
-    let current = html;
-    do {
-        debugger; // tslint:disable-line
-        current = html.replace(/(<[a-z]+ +)(([a-z]+=(('[^']*')|("[^"]*")) +)*)(([a-z]+)=([^'"> ]+))/gi, "$1$2$8='$9'");
-    } while (false);
-    return current;
+    return multiRun(s => s
+        .replace(/(<[a-z]+\s+)(([a-z]+=(['"])[^4]*\4\s+)*)(([a-z]+)=([^'">\s]+))/gi, '$1$2$6="$7"')
+    )(html);
 }
 
 export function fixUnquotedAttribute(attrName: string) {
     return (html: string) => html
-        .replace(new RegExp(`${attrName}=([^'"][^ >]*)`, 'gi'), `${attrName}='$1'`)
+        .replace(new RegExp(`${attrName}=([^'"][^\s>]*)`, 'gi'), `${attrName}='$1'`)
         ;
 }
 
 function fixBooleanAttributes(html: string) {
     return html
-        .replace(new RegExp(`(<[a-z]+ +)(([a-z]+=[^ ] +)*)([a-z]+) `, 'gi'), `$1$2$4='true' `)
+        .replace(/(<[a-z]+\s+)(([a-z]+=[^\s]+\s+)*)([a-z]+)(\s|>|\/)/gi, `$1$2$4='true'$5`)
         ;
+}
+
+function fixUnescapedAmpersand(html: string) {
+    return multiRun(s => s
+        .replace(/&([^a]|a[^m]|am[^p]|amp[^;])/gi, "&amp;$1")
+    )(html);
 }
 
 function fixSingleTag(tagName: string) {
@@ -36,23 +52,37 @@ function fixSingleTag(tagName: string) {
 
 function fixInvalidComments(html: string) {
     return html
-        .replace(new RegExp('<!---+([^->]*)-*>', 'g'), '<!-- $1 -->')
+        .replace(/<!---+([^->]*)-*>/gi, '<!-- $1 -->')
+        ;
+}
+
+function fixCharEntity(html: string) {
+    return multiRun(s => s
+        .replace(/&#(\d{1,4})(&|<)/g, '&#$1;$2')
+    )(html);
+}
+
+function fixSpecialCaseAzLibRu(html: string) {
+    return html
+        .replace('Статистика.</a></div>', 'Статистика.</a>')
+        .replace(
+            '<table align=center width=90% border=0 cellspacing=10><td align=center><font size=-1>',
+            '<table align=center width=90% border=0 cellspacing=10><td align=center /><font size=-1>')
+        .replace(/<ul><font color="#555555">\s*<\/ul><\/font>/gi, '<ul><font color="#555555"></font></ul>')
         ;
 }
 
 export const htmlToWellFormedXml = combineFs(
     fixSingleTag('br'),
     fixSingleTag('hr'),
-    // fixSingleTag('tr'),
-    // fixSingleTag('td'),
     fixSingleTag('li'),
-    // fixUnquotedAttribute('href'),
-    // fixUnquotedAttribute('size'),
-    // fixUnquotedAttribute('width'),
-    // fixUnquotedAttribute('border'),
-    // fixUnquotedAttribute('cellPadding'),
-    // fixUnquotedAttribute('cellSpacing'),
+    fixSingleTag('input'),
+    fixSingleTag('dd'),
+    fixSingleTag('pre'),
+    fixUnescapedAmpersand,
     fixAllUnquotedAttributes,
     fixBooleanAttributes,
     fixInvalidComments,
+    fixCharEntity,
+    fixSpecialCaseAzLibRu,
 );
