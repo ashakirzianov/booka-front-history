37123933d2bf942f271aae81923ab5497ebb3c8c
Anton Shakirzianov <ashakirzianov@icloud.com>
2018-04-06 22:57:12 -0700

    Interface for iterators

--- src/xmlProcessing/xmlIterator.ts
+++ src/xmlProcessing/xmlIterator.ts
@@ -1,17 +1,31 @@
 import { Thunk } from "../utils";
-import { XmlNode, XmlNodeDocument, XmlNodeWithChildren, hasChildren } from "./xmlNode";
+import { XmlNode, XmlNodeDocument, hasChildren, isElement } from "./xmlNode";
 
 export type XmlIteratorValue = {
     prevSibling: Thunk<XmlIterator>,
     nextSibling: Thunk<XmlIterator>,
-    children: Thunk<XmlIterator>,
+    child: Thunk<XmlIterator>,
     node: XmlNode,
     parent: Thunk<XmlIterator>,
 };
 export type XmlIterator = { value: XmlIteratorValue, done: false } | { done: true };
 
-const doneIterator: XmlIterator = { done: true };
-const nullIterator: Thunk<XmlIterator> = () => doneIterator;
+export const doneIterator: XmlIterator = { done: true };
+export const nullIteratorThunk: Thunk<XmlIterator> = () => doneIterator;
+
+export function name(xi: XmlIterator) {
+    return !xi.done && isElement(xi.value.node) ? xi.value.node.name : undefined;
+}
+
+export function attributes(xi: XmlIterator) {
+    return !xi.done && isElement(xi.value.node) ? xi.value.node.attributes : undefined;
+}
+
+export function child(xi: XmlIterator) {
+    const children = !xi.done ? xi.value.child() : undefined;
+    return children && !children.done ? children : undefined;
+}
+
 function make(value: XmlIteratorValue): XmlIterator {
     return {
         value,
@@ -21,17 +35,17 @@ function make(value: XmlIteratorValue): XmlIterator {
 
 export function rootIterator(root: XmlNodeDocument): XmlIterator {
     return make({
-        prevSibling: nullIterator,
-        nextSibling: nullIterator,
-        parent: nullIterator,
-        children: buildChildIteratorThunk(root.children),
+        prevSibling: nullIteratorThunk,
+        nextSibling: nullIteratorThunk,
+        parent: nullIteratorThunk,
+        child: buildChildIteratorThunk(root.children),
         node: root,
     });
 }
 
 function buildIteratorThunk(node: XmlNode | undefined): Thunk<XmlIterator> {
     if (!node) {
-        return nullIterator;
+        return nullIteratorThunk;
     }
 
     if (node.type === 'document') {
@@ -45,9 +59,9 @@ function buildIteratorThunk(node: XmlNode | undefined): Thunk<XmlIterator> {
 function buildChildIteratorThunk(siblings: XmlNode[], index: number = 0): Thunk<XmlIterator> {
     const node = siblings[index];
     return () => make({
-        prevSibling: index > 0 ? buildChildIteratorThunk(siblings, index - 1) : nullIterator,
-        nextSibling: index < siblings.length - 1 ? buildChildIteratorThunk(siblings, index + 1) : nullIterator,
-        children: hasChildren(node) ? buildChildIteratorThunk(node.children) : buildChildIteratorThunk([]),
+        prevSibling: index > 0 ? buildChildIteratorThunk(siblings, index - 1) : nullIteratorThunk,
+        nextSibling: index < siblings.length - 1 ? buildChildIteratorThunk(siblings, index + 1) : nullIteratorThunk,
+        child: hasChildren(node) ? buildChildIteratorThunk(node.children) : buildChildIteratorThunk([]),
         parent: buildIteratorThunk(node.parent),
         node: node,
     });
