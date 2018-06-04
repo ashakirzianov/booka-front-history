5f47a97295df768dd134dbd1f76b772add2a8489
ashakirzianov <ashakirzianov@icloud.com>
2018-06-04 13:16:35 -0700

    Count information in book node rendering

--- src/components/BookComp.tsx
+++ src/components/BookComp.tsx
@@ -10,32 +10,36 @@ const ParagraphComp: Comp<{ p: Paragraph }> = props =>
 const SubpartComp: Comp<Subpart> = props =>
     <Column>
         <SubpartTitle text={props.title} />
-        {props.content.map(bn => <BookNodeComp node={bn} />)}
+        {buildNodes(props.content)}
     </Column>;
 
 const PartComp: Comp<Part> = props =>
     <Column>
         <PartTitle text={props.title} />
-        {props.content.map(bn => <BookNodeComp node={bn} />)}
+        {buildNodes(props.content)}
     </Column>;
 
 const ChapterComp: Comp<Chapter> = props =>
     <Column>
         <ChapterTitle text={props.title} />
-        {props.content.map(bn => <BookNodeComp node={bn} />)}
+        {buildNodes(props.content)}
     </Column>;
 
-const BookNodeComp: Comp<{ node: BookNode }> = props =>
+const BookNodeComp: Comp<{ node: BookNode, count: number }> = props =>
     isParagraph(props.node) ? <ParagraphComp p={props.node} />
         : props.node.kind === 'chapter' ? <ChapterComp {...props.node} />
             : props.node.kind === 'part' ? <PartComp {...props.node} />
                 : props.node.kind === 'subpart' ? <SubpartComp {...props.node} />
-                    : assertNever(props.node);
+                    : assertNever(props.node, props.count.toString());
 
 const BookComp: Comp<Book> = props =>
     <Column>
         <BookTitle text={props.title} />
-        {props.content.map(bn => <BookNodeComp node={bn} />)}
+        {buildNodes(props.content)}
     </Column>;
+
+function buildNodes(nodes: BookNode[]) {
+    return nodes.map((bn, i) => <BookNodeComp node={bn} count={i}/>);
+}
 
 export { BookComp };
