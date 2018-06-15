049a7912e59367400c467f0db7382a872b99a314
ashakirzianov <ashakirzianov@icloud.com>
2018-06-15 16:28:07 -0700

    Async loadBook

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -2,9 +2,21 @@ import { buildPartialReducer } from "./redux-utils";
 import { ActionsTemplate } from "../model/actions";
 import { combineReducers } from "./react-redux-utils";
 import { App } from "../model/app";
+import { string2book } from "../xmlProcessing/azLibRu";
+import { text } from "../samples/warAndPeaceShort";
+import { Book } from "../model/book";
 
 export function loadBoolean(x: number): Promise<{ helloBaz: boolean}> {
     return new Promise((res, rej) => res({ helloBaz: true }));
+}
+
+export function loadBook(): Promise<Book> {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            const b = string2book(text);
+            res(b);
+        });
+    });
 }
 
 const book = buildPartialReducer<App['book'], ActionsTemplate>({
