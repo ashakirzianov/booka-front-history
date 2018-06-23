59b13fed46bba8fedac702c27e922bfb8fbdc394
ashakirzianov <ashakirzianov@icloud.com>
2018-06-22 17:24:51 -0700

    Using 'CORS Anywhere' proxy

--- src/loader/htmlLoad.ts
+++ src/loader/htmlLoad.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const corsAnywhereProxy = 'https://cors-anywhere.herokuapp.com/';
 export function loadHtml(url: string): Promise<string> {
     return axios
-        .get(url)
+        .get(corsAnywhereProxy + url)
         .then(response => {
             return response.data;
         });
