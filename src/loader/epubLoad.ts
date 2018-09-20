9458e4085271fd8012fa5506499778160656fea0
ashakirzianov <ashakirzianov@icloud.com>
2018-09-20 15:19:39 +0300

    Open epub from data

--- src/loader/epubLoad.ts
+++ src/loader/epubLoad.ts
@@ -1,16 +1,15 @@
 import axios from 'axios';
+import epubParser from '@gxl/epub-parser';
 
 export function loadStaticEpub(fileName: string): Promise<any> {
     return axios
-        .get(fileName)
+        .get(fileName, {
+            responseType: 'arraybuffer',
+        })
         .then(response => {
-            const url = window.URL.createObjectURL(new Blob([response.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', fileName);
-            document.body.appendChild(link);
-            link.click();
-            return response;
+            const buffer = new Buffer(response.data);
+            const epub = epubParser(buffer);
+            return epub;
         })
         ;
 }
