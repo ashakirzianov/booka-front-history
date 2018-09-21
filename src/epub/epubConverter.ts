b26bbd171b1bce4980e6f0760b33b5ac77dedde5
ashakirzianov <ashakirzianov@icloud.com>
2018-09-21 16:18:44 +0300

    Minor refactoring

--- src/epub/epubConverter.ts
+++ src/epub/epubConverter.ts
@@ -1,16 +1,26 @@
 import epubParser from '@gxl/epub-parser';
 import { Book } from '../model/book';
+// tslint:disable-next-line:no-submodule-imports
+import { Epub } from '@gxl/epub-parser/build/types/epubParser';
 
-export function convertEpubArrayBuffer(arrayBuffer: ArrayBuffer): Promise<Book> {
+export function arrayBuffer2epub(arrayBuffer: ArrayBuffer): Promise<Epub> {
+    const buffer = new Buffer(arrayBuffer);
+    return epubParser(buffer);
+}
+
+// TODO: make promise ?
+export function epub2book(epub: Epub): Book {
+    return {
+        kind: 'book' as 'book',
+        title: epub.info.title,
+        author: epub.info.author,
+        content: epub.sections.slice(0, 10).map(section =>
+            (section.toMarkdown ? section.toMarkdown() : "NOPE")),
+    };
+}
+
+export function arrayBuffer2book(arrayBuffer: ArrayBuffer): Promise<Book> {
     const buffer = new Buffer(arrayBuffer);
     return epubParser(buffer)
-        .then(epub => {
-            return {
-                kind: 'book' as 'book',
-                title: epub.info.title,
-                author: epub.info.author,
-                content: epub.sections.slice(0, 10).map(section =>
-                    (section.toMarkdown ? section.toMarkdown() : "NOPE")),
-            };
-        });
+        .then(epub2book);
 }
