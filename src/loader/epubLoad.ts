4a6f8a767c964a10eeaa66d4bb4a8b21dbc5632a
ashakirzianov <ashakirzianov@icloud.com>
2018-10-28 14:40:38 +0400

    Make 'loadStaticEpub' async

--- src/loader/epubLoad.ts
+++ src/loader/epubLoad.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { arrayBuffer2book } from '../epub/epubConverter';
 import { Book, createErrorBook } from '../model/book';
 
-export function loadStaticEpub(fileName: string): Promise<Book> {
-    return axios
-        .get(fileName, {
+export async function loadStaticEpub(fileName: string): Promise<Book> {
+    try {
+        const response = await axios.get(fileName, {
             responseType: 'arraybuffer',
-        })
-        .then(response => {
-            return arrayBuffer2book(response.data);
-        })
-        .catch(reason => createErrorBook("Can't find static book: " + fileName));
+        });
+        return arrayBuffer2book(response.data);
+    } catch (reason) {
+        return createErrorBook("Can't find static book: " + fileName);
+    }
 }
