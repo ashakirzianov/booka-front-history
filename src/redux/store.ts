1c598d0e4d52886b58b508ccb0c7b7c35ce13a82
ashakirzianov <ashakirzianov@icloud.com>
2018-10-30 19:15:04 +0400

    Uninstall 'redux-logger'

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from "redux";
-// import { logger } from "redux-logger"; // TODO: remove redux-logger ?
 import { throttle } from "lodash";
 import { reducer } from "./reducers";
 import { State, storeState, restoreState } from "./storage";
@@ -7,8 +6,6 @@ import promiseMiddleware from 'redux-promise-middleware';
 import { createBrowserHistory, Location, Action } from "history";
 import { BookLocator, staticBookLocator } from "../model/bookLocator";
 import { loadBL } from "./api";
-
-// TODO: hide this preparations behind some interface?
 
 function validateState(restored: State | undefined) {
     return undefined;
@@ -22,7 +19,7 @@ function createNewState(): State {
 
 const initial: State = validateState(restoreState()) || createNewState();
 export const store = createStore(reducer, initial, applyMiddleware(
-    promiseMiddleware(), // TODO: consider removing promise support?
+    promiseMiddleware(),
 ));
 
 store.subscribe(throttle(() => {
