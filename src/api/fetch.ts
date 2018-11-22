2d35ec66d7888cfa76432b2084166be5c6977679
ashakirzianov <ashakirzianov@icloud.com>
2018-11-22 13:52:08 +0400

    Remove epub fetching related code

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,66 +1,31 @@
 import axios from 'axios';
 import { Book, noBook, errorBook, BookLocator } from "../model";
-import { arrayBuffer2book } from "../epub";
 
 const backendBase = 'https://reader-back.herokuapp.com';
-const epubPath = '/epub';
 const jsonPath = '/json';
 
 export async function fetchBL(bookLocator: BookLocator): Promise<Book> {
     switch (bookLocator.bl) {
         case 'no-book':
             return Promise.resolve(noBook());
-        case 'static-epub':
-            return fetchStaticEpub(bookLocator.name);
         case 'static-book':
-            return fetchStaticBook(bookLocator.name);
+            return fetchBook(bookLocator.name);
         default:
             return Promise.resolve(noBook());
     }
 }
 
-export async function fetchStaticBook(fileName: string): Promise<Book> {
+export async function fetchBook(bookName: string): Promise<Book> {
     try {
-        const response = await fetchStaticJson(jsonPath + fileName);
+        const response = await fetchJson(backendBase + jsonPath + bookName);
         return response as Book;
     } catch (reason) {
-        return errorBook("Can't find static book: " + fileName);
+        return errorBook("Can't find static book: " + bookName);
     }
 }
 
-export async function fetchStaticEpub(fileName: string): Promise<Book> {
-    try {
-        const buffer = await fetchStaticBuffer(epubPath + fileName);
-        return arrayBuffer2book(buffer);
-    } catch (reason) {
-        return errorBook("Can't find static book: " + fileName);
-    }
-}
-
-export async function fetchHtml(url: string): Promise<string> {
-    const corsAnywhereProxy = 'https://cors-anywhere.herokuapp.com/';
-    const response = await axios
-        .get(corsAnywhereProxy + url);
-    return response.data;
-}
-
-export async function fetchStaticString(fileName: string): Promise<string> {
-    const response = await axios
-        .get(fileName);
-    return response.data;
-}
-
-export async function fetchStaticBuffer(fileName: string): Promise<ArrayBuffer> {
-    const response = await axios
-        .get(backendBase + fileName, {
-            responseType: 'arraybuffer',
-        })
-        ;
-    return response.data;
-}
-
-export async function fetchStaticJson(fileName: string): Promise<object> {
-    const json = await axios.get(backendBase + fileName, {
+export async function fetchJson(url: string): Promise<object> {
+    const json = await axios.get(url, {
         responseType: 'json',
     });
 
--- src/model/bookLocator.ts
+++ src/model/bookLocator.ts
@@ -1,25 +1,14 @@
 export type NoBookLocator = { bl: 'no-book' };
-export type StaticEpubLocator = {
-    bl: 'static-epub',
-    name: string,
-};
 export type StaticBookLocator = {
     bl: 'static-book',
     name: string,
 };
 
-export type BookLocator = NoBookLocator | StaticEpubLocator | StaticBookLocator;
+export type BookLocator = NoBookLocator | StaticBookLocator;
 
 export function noBookLocator(): NoBookLocator {
     return {
         bl: 'no-book',
-    };
-}
-
-export function staticEpubLocator(name: string): StaticEpubLocator {
-    return {
-        bl: 'static-epub',
-        name: name,
     };
 }
 
