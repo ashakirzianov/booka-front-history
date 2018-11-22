56d861416acc8dc0761146704c15dc96848489ff
ashakirzianov <ashakirzianov@icloud.com>
2018-11-22 13:54:49 +0400

    Remove epub parsing related code

--- src/epub/epubParser.ts
+++
@@ -1,6 +0,0 @@
-import epubParser from '@gxl/epub-parser';
-import { PromiseType } from '../utils';
-
-export type Epub = PromiseType<ReturnType<typeof epubParser>>;
-export type Section = Epub['sections'][0];
-export { epubParser };
